f2422671250bb1b0b9d8e8eb2e0366369e5a5c36
Thor10 <btmngmt@gmail.com>
2018-09-17 18:58:50 +0100

    Language Update
    Spell Checked & Reworded

--- language/admin/install/english.php
+++ language/admin/install/english.php
@@ -2,10 +2,10 @@
 
 /**
 **********************
-** BTManager v3.0.1 **
+** BTManager v3.0.2 **
 **********************
 ** http://www.btmanager.org/
-** https://github.com/blackheart1/BTManager
+** https://github.com/blackheart1/BTManager3.0.2
 ** http://demo.btmanager.org/index.php
 ** Licence Info: GPL
 ** Copyright (C) 2018
@@ -13,11 +13,11 @@
 ** Created By Antonio Anzivino (aka DJ Echelon)
 ** And Joe Robertson (aka joeroberts)
 ** Project Leaders: Black_heart, Thor.
-** File install/english.php 2018-08-19 09:26:00 Black_heart
+** File install/english.php 2018-09-18 18:54:00 Thor
 **
 ** CHANGES
 **
-** 2018-08-19 - Created New
+** 2018-09-18 - Created New
 **/
 
 if (!defined('IN_PMBT'))
@@ -32,26 +32,32 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'INTRO_UPGRADE'					=> 'Welcome to BTManager Upgrade system',
-	'INTRO_UPGRADE_EXP'				=> 'This Upgrade system well check your data base and find andy missing tables and add them as needed<br />if a table is missing a colom row or if one dose not match then it well try to add the missing colom or convert yours as needed to run the new Release.',
-	'NOTICE_DB_CHANGES_ND'			=> 'We have checked your data base and found<br />You have <strong>%1$s</strong> Table that needs added<br /><strong>%2$s</strong> New Rows Need added<br /><strong>%3$s</strong> Rows that need updated',
-	'BACKUP_NOTICE'					=> 'Please backup your board before updating in case any problems arise during the update process.',
-	'START_BACKUP'					=> 'Start backup',
-	'NO_BACKUP'						=> 'Proseed with out Back Up',
-	'BACKUP_SUCCESS'				=> 'The backup file has been created successfully.',
-	'FILE_WRITE_FAIL'				=> 'Unable to write file to storage folder.',
-	'RETRY_BACKUP'					=> 'Try to back up again!',
-	'PROCEED_WITH_UD'				=> 'Proceed with update.',
-	'INLINE_UPDATE_SUCCESSFUL'		=> 'The database update was successful.',
-	'FINAL_SETS'					=> 'Now that the Update is complete you can Now<br />
-	<li>Update your files with the Newest release of BT.Manager</li>
-	<li>If you had any mods on your site you well need to reinstall them.</li>
-	<ol><li>After you have updated all your files you well need to procede to the admin panel and update your</li>
-	<li>Groups Permissions</li>
-	<li>This well include all users with modified Permissions</li>
-	<li>Administrator Roles</li>
-	<li>Moderator Roles</li>
-	<li>User Roles</li></ol>',
+    'INTRO_UPGRADE'                 => 'Welcome to the BT.Manager Upgrade System',
+
+    'INTRO_UPGRADE_EXP'             => 'The Upgrade System will Scan your Database to Check for any Missing Tables and will attempt to Add them.<br />If a Table is Missing a Column/Row or if one Does Not Match it will then try to Add them or Convert yours in order for you to Install the New Release.',
+
+    'NOTICE_DB_CHANGES_ND'          => 'We have Scanned your Database and found:-<br />You have <strong>%1$s</strong> Table/s that need Adding<br /><strong>%2$s</strong>New Rows that need Adding<br /><strong>%3$s</strong>Rows that need Updating',
+
+    'BACKUP_NOTICE'                 => 'Please Backup your Site before Updating just in case any problems arise during the Update Process.',
+
+    'START_BACKUP'                  => 'Start Backup',
+    'NO_BACKUP'                     => 'Proceed without Backup',
+    'BACKUP_SUCCESS'                => 'The Backup File has been Successfully Created.',
+    'FILE_WRITE_FAIL'               => 'We are Unable to Write the File to your Storage Folder.',
+    'RETRY_BACKUP'                  => 'Try to Backup again?',
+    'PROCEED_WITH_UD'               => 'Proceed with Update?',
+    'INLINE_UPDATE_SUCCESSFUL'      => 'The Database Update was Successful.',
+    'FINAL_SETS'                    => 'Now that the Update is Complete you can now:-<br />
+
+    <li>Update your Files with the Newest Release of BT.Manager</li>
+    <li>If you had any Mods Installed on your Site you will need to Reinstall them.</li>
+    <ol><li>After you have Updated ALL your Files you will need to Proceed to the Usergroup Management in your Administration Panel and Update your:-</li>
+    <li>Groups Permissions</li>
+    <li>This will Include ALL Users with Modified Permissions</li>
+    <li>Administrator Roles</li>
+    <li>Moderator Roles</li>
+    <li>User Roles</li>
+    <li>Forum Roles</li></ol>',
 ));
 
 ?>
